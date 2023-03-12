e43d498e065fa6662428dde6a0e2c610a4566156
yunjun199321 <yunjun199321@gmail.com>
2023-03-12 21:59:41 +0800

    Websocket is working with chatgpt

--- index.js
+++ index.js
@@ -1,51 +1,94 @@
-let submit = document.getelementbyid("submit"),
- pl = document.getelementbyid("pl");
+const submit = document.getElementById("submit");
+const pl = document.getElementById("pl");
+
 // 很重要 必须写，判断浏览器是否支持websocket
-let createwebsocket = (() => {
+const createwebsocket = (() => {
  return (urlvalue) => {
   if (window.websocket) return new websocket(urlvalue);
   if (window.mozwebsocket) return new mozwebsocket(urlvalue);
   return false;
  }
 })()
-// 实例化websoscket websocket有两种协议ws(不加密)和wss(加密)
-let websocket = createwebsocket(`ws://127.0.0.1:3000`);
-websocket.onopen = evt => {
- addmsg(1, '你好，欢迎进入实时聊天室！')
-}
-websocket.onmessage = evt => {
- // 这是服务端返回的数据
- addmsg(1, evt.data);
- submit.innerhtml = '发送';
-}
+
+// // 实例化websoscket websocket有两种协议ws(不加密)和wss(加密)
+// let websocket = createwebsocket(`ws://127.0.0.1:3000`);
+// websocket.onopen = evt => {
+//  addmsg(1, '你好，欢迎进入实时聊天室！')
+// }
+// websocket.onmessage = evt => {
+//  // 这是服务端返回的数据
+//  addmsg(1, evt.data);
+//  submit.innerhtml = '发送';
+// }
 // input事件发送数据
-submit.onclick = (e) => {
- if (e.target.innerhtml == '回复中...') {
-  return false
- }
- e.target.innerhtml = '回复中...';
- const str = document.getelementbyid("pl").value;
- websocket.send(str);
- addmsg(2, str);
-}
-// 绑定回车事件
-function keyenter() {
- if (event.keycode == 13) {
-  document.getelementbyid("submit").click();
- }
-}
+// submit.onclick = (e) => {
+//  if (e.target.innerhtml == '回复中...') {
+//   return false
+//  }
+//  e.target.innerhtml = '回复中...';
+//  const str = document.getelementbyid("pl").value;
+//  websocket.send(str);
+//  addmsg(2, str);
+// }
+
 // 添加消息
 function addmsg(type, msg) {
- let li = document.createelement('li');
- // 1机器人/2自己
+ const li = document.createElement('li');
+ // 1: Chatbot, 2: User
  if (type == 1) {
-  li.classlist.add('computer-say');
-  li.innerhtml = `<span class="sayman">机器人</span><span class="computer say">${msg}</span>`;
+  li.classList.add('computer-say');
+  li.innerHTML = `<span class="sayman">HKGPT</span><span class="computer say">${msg}</span>`;
  } else {
-  li.classlist.add('my-say');
-  li.innerhtml = `<span class="computer say">${msg}</span><span class="sayman">我</span>`;
+  li.classList.add('my-say');
+  li.innerHTML = `<span class="computer say">${msg}</span><span class="sayman">Me</span>`;
   pl.value = '';
  }
- document.getelementbyid('view').appendchild(li);
- document.getelementbyid('ulview').scrollto(0, document.getelementbyid('view').clientheight);
+ document.getElementById('view').appendChild(li);
+ document.getElementById('ulview').scrollTo(0, document.getElementById('view').clientHeight);
+}
+
+// Create websocket connection.
+const socket = new WebSocket('ws://localhost:3000');
+// socket.onopen = evt => {
+//  addmsg(1, '你好，欢迎进入实时聊天室！')
+// }
+
+socket.onmessage = evt => {
+    // 这是服务端返回的数据
+    addmsg(1, evt.data);
+    submit.innerHTML = '发送';
+   }
+   // input事件发送数据
+   submit.onclick = (e) => {
+    if (e.target.innerHTML == '回复中...') {
+     return false
+    }
+    e.target.innerHTML = '回复中...';
+    const str = document.getElementById("pl").value;
+    socket.send(str);
+    addmsg(2, str);
+   }
+
+// Connection opened
+socket.addEventListener('open', function (event) {
+    console.log('Connected to WS server');
+});
+
+// Listen for message
+socket.addEventListener('message', function (event) {
+    console.log('Message from server ', event.data);
+});
+
+// Send message to server
+const sendMessage = () => {
+    socket.send('Hello from Client!');
+}
+
+// Listen to Enter key press event
+function keyenter() {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        submit.click();
+        pl.value = '';
+    }
 }
--- server.js
+++ server.js
@@ -1,31 +1,70 @@
-const koa = require('koa'),
-  route = require('koa-route'),
-  websockify = require('koa-websocket'),
-  http = require('http'),
-  app = websockify(new koa());
- 
-app.ws.use(route.all('/', ctx => {
- // websocket作为“ctx.websocket”添加到上下文中。
- ctx.websocket.on('message', message => {
-  startrequest(message, ctx);
- });
-}));
- 
-function startrequest(message, ctx) {
- // 采用http模块向服务器发起一次get请求  
- http.get(`http://api.qingyunke.com/api.php?key=free&appid=0&msg=${encodeuri(message)}`, res => {
-  // 防止中文乱码
-  res.setencoding('utf-8');
-  // 监听data事件，每次取一块数据
-  res.on('data', chunk => {
-   ctx.websocket.send(json.parse(chunk).content);
-  });
- }).on('error', err => {
-  ctx.websocket.send('对不起，网络故障了');
- });}
- 
-// 监听端口、启动程序
-app.listen(3000, err => {
- if (err) throw err;
- console.log('websocket服务器启动在3000端口');
-})
+// const koa = require('koa'),
+//   route = require('koa-route'),
+//   websockify = require('koa-websocket'),
+//   http = require('http'),
+//   app = websockify(new koa());
+
+// app.ws.use(route.all('/', ctx => {
+//  // websocket作为“ctx.websocket”添加到上下文中。
+//  ctx.websocket.on('message', message => {
+//   startrequest(message, ctx);
+//  });
+// }));
+
+// function startrequest(message, ctx) {
+//  // 采用http模块向服务器发起一次get请求  
+//  http.get(`http://api.qingyunke.com/api.php?key=free&appid=0&msg=${encodeuri(message)}`, res => {
+//   // 防止中文乱码
+//   res.setencoding('utf-8');
+//   // 监听data事件，每次取一块数据
+//   res.on('data', chunk => {
+//    ctx.websocket.send(json.parse(chunk).content);
+//   });
+//  }).on('error', err => {
+//   ctx.websocket.send('对不起，网络故障了');
+//  });}
+
+// // 监听端口、启动程序
+// app.listen(3000, err => {
+//  if (err) throw err;
+//  console.log('websocket服务器启动在3000端口');
+// })
+
+const WebSocket = require('ws');
+const WebSocketServer = WebSocket.Server;
+const wss = new WebSocketServer({
+    port: 3000
+});
+
+wss.on('connection', function (ws) {
+    console.log(`[SERVER] connection()`);
+    ws.send('Welcome! 我是智能导游，能帮你回答香港旅游相关问题。我支持多种语言。');
+    ws.on('message', function (message) {
+        console.log(`[SERVER] Received: ${message}`);
+        ws.send('Got your message: ' + message);
+
+        // ChatGPT code
+        // const { Configuration, OpenAIApi } = require("openai");
+
+        // const configuration = new Configuration({
+        //     apiKey: "rrs3SgvHdH8iGgUIdlwPT3BlbkFJ7t6lkIrRdviNEU0KrXNw",
+        // });
+        // const openai = new OpenAIApi(configuration);
+
+        // const completion = await openai.createChatCompletion({
+        //     model: "gpt-3.5-turbo",
+        //     messages: [{ role: "user", content: "Hello world" }],
+        // });
+        // console.log(completion.data.choices[0].message);
+
+        setTimeout(() => {
+            ws.send(`What's your name?`, (err) => {
+                if (err) {
+                    console.log(`[SERVER] error: ${err}`);
+                }
+            });
+        }, 1000);
+    })
+});
+
+console.log('ws server started at port 3000...');
